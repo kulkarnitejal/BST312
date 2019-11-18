629f7d9d75f99d23d30be83744bf801df7a506d2
Tejal Kulkarni <45702370+kulkarnitejal@users.noreply.github.com>
2019-11-17 18:09:32 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,89 @@
+// Sample driver
+// EE 312 Fall 2018
+//
+// The BST should work for any data type or object.
+// Other user created objects must have the << operator overloaded for
+// output.
+//
+
+#include <iostream>
+#include <fstream>
+#include "BST312.h"
+
+using namespace std;
+
+template <typename T>
+void print(vector<T> vec){
+    for(int i = 0 ; i < vec.size() ; i++){
+        cout << vec[i] << endl;;
+    }
+}
+
+int main2() {
+
+    ifstream inFile;
+    inFile.open("test.txt");
+
+    BST_312<string> bst;
+
+    string s;
+    inFile >> s;
+    while (inFile) {;
+        cout << "inserting ... " << s << endl;
+        bst.insertItem(s);
+        inFile >> s;
+    }
+
+    cout << "postorder traversal is " << endl;
+    print(bst.postOrderTraversal());
+    cout << endl;
+
+    cout << "preorder traversal is " << endl;
+    print(bst.preOrderTraversal());
+    cout << endl;
+
+    cout << "inorder traversal is " << endl;
+    print(bst.inOrderTraversal());
+    cout << endl;
+
+    cout << "Remove items " << endl;
+    cout << "number of nodes in tree before delete is " << bst.countNodes() << endl;
+    s = "bst";
+    bst.deleteItem(s);
+    print(bst.postOrderTraversal());
+    cout << endl;
+    cout << "number of nodes in tree after delete is " << bst.countNodes() << endl;
+    cout << endl;
+
+}
+
+int main() {
+    BST_312<int> bst;
+
+    cout<<"empty bst size" << bst.countNodes() <<endl;
+
+    bst.insertItem(2);
+    bst.insertItem(5);
+    bst.insertItem(8);
+    bst.insertItem(6);
+    bst.insertItem(7);
+
+    cout << "size: " << bst.countNodes() <<endl;
+
+    bst.insertItem(5);
+    cout << "size w add of same node: " <<bst.countNodes()<< endl;
+
+    cout << "preorder traversal is " << endl;
+    print(bst.postOrderTraversal());
+    cout << endl;
+
+    cout << "Remove items " << endl;
+    cout << "number of nodes in tree before delete is " << bst.countNodes() << endl;
+    bst.deleteItem(5);
+    print(bst.postOrderTraversal());
+    cout << endl;
+    cout << "number of nodes in tree after delete is " << bst.countNodes() << endl;
+    cout << endl;
+}
+
+
